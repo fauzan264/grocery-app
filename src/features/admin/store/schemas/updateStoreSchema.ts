d665eaff7344da1b9fbb5aa0a00702c4c6252d2b
Fauzan <fauzan.c4@gmail.com>
2025-09-29 03:27:55 +0700

    fix: fix schema update store logo

--- src/features/admin/store/schemas/updateStoreSchema.ts
+++ src/features/admin/store/schemas/updateStoreSchema.ts
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+const maximumFileSize = 1024 * 1024;
+const fileFormatAccepted = ["image/jpeg", "image/png", "image/gif"];
+
 export const updateStoreSchema = yup.object().shape({
   name: yup.string(),
   description: yup.string(),
@@ -9,5 +12,15 @@ export const updateStoreSchema = yup.object().shape({
   address: yup.string(),
   latitude: yup.string(),
   longitude: yup.string(),
-  logo: yup.string(),
+  logo: yup
+    .mixed<File>()
+    .nullable()
+    .test("fileSize", "Maximum file is 1 MB", (file) => {
+      if (!file) return true;
+      return file.size <= maximumFileSize;
+    })
+    .test("fileFormat", "Format file not accepted", (file) => {
+      if (!file) return true;
+      return fileFormatAccepted.includes(file.type);
+    }),
 });
