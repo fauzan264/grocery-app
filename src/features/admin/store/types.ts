b44db182163122a74ef820f7163ee890989c8d41
Fauzan <fauzan.c4@gmail.com>
2025-09-29 03:29:20 +0700

    fix: fix type IStore

--- src/features/admin/store/types.ts
+++ src/features/admin/store/types.ts
@@ -2,12 +2,12 @@ export interface IStore {
   id?: string;
   name?: string;
   description?: string;
-  province_id?: number;
-  city_id?: number;
-  district_id?: number;
+  provinceId?: number;
+  cityId?: number;
+  districtId?: number;
   address?: string;
   latitude?: string;
   longitude?: string;
-  logo?: string;
+  logo?: File | null;
   status?: string;
 }
