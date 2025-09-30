25a0f527771e7e49d36750e6731363f49c47e570
Fauzan <fauzan.c4@gmail.com>
2025-09-30 12:06:16 +0700

    refactor: move button edit and change password to component profile

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -1,27 +1,30 @@
+"use client";
 import UserProfileComponent from "@/features/user/components/UserProfileComponent";
-import Link from "next/link";
+import { IUser } from "@/features/user/type";
+import { myProfile } from "@/services/user";
+import useAuthStore from "@/store/useAuthStore";
+import camelcaseKeys from "camelcase-keys";
+import { useEffect, useState } from "react";
 
 export default function ProfileAdminPage() {
+  const { token } = useAuthStore();
+  const [profile, setProfile] = useState<IUser | undefined>(undefined);
+
+  const onGetProfile = async ({ token }: { token: string }) => {
+    const response = await myProfile({ token });
+
+    setProfile(camelcaseKeys(response.data.data));
+  };
+
+  useEffect(() => {
+    if (token) {
+      onGetProfile({ token });
+    }
+  }, [token]);
   return (
     <div className="mx-auto my-10 w-11/12 h-full">
-      <div className="flex">
-        <h1 className="text-2xl text-gray-700">Profile Management</h1>
-        <div className="ml-auto">
-          <Link
-            href={`/admin/profile/change-password`}
-            className="btn btn-sm ml-auto bg-emerald-500 text-white text-sm rounded-md hover:bg-emerald-600 active:bg-emerald-600 transition ease-in-out duration-300 focus:outline-none focus:ring-0 border-0 mr-1"
-          >
-            Change Password
-          </Link>
-          <Link
-            href={`/admin/profile/edit`}
-            className="btn btn-sm ml-auto bg-amber-400 text-white text-sm rounded-md hover:bg-amber-500 active:bg-amber-500 transition ease-in-out duration-300 focus:outline-none focus:ring-0 border-0"
-          >
-            Edit
-          </Link>
-        </div>
-      </div>
-      <UserProfileComponent />
+      <h1 className="text-2xl text-gray-700">Profile Management</h1>
+      <UserProfileComponent profile={profile} />
     </div>
   );
 }
