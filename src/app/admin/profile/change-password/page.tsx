a08cb2082f197fb3fac969ebfb6e3034b997daeb
Fauzan <fauzan.c4@gmail.com>
2025-09-30 12:02:58 +0700

    refactor: update input and button use component

--- src/app/admin/profile/change-password/page.tsx
+++ src/app/admin/profile/change-password/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import Button from "@/components/ui/button";
+import FormInput from "@/components/form/FormInput";
 import { changePasswordSchema } from "@/features/auth/change-password/schemas/changePasswordSchema";
 import { changePassword } from "@/services/auth";
 import useAuthStore from "@/store/useAuthStore";
 import { AxiosError } from "axios";
 import { useFormik } from "formik";
 import { useRouter } from "next/navigation";
-import { TbLockPassword } from "react-icons/tb";
 import { toast } from "react-toastify";
 
 export default function ChangePasswordPage() {
@@ -56,74 +57,29 @@ export default function ChangePasswordPage() {
 
   return (
     <div className="mx-auto my-10 w-11/12 min-h-full">
-      <h1 className="text-2xl text-gray-700">Create Store</h1>
-      <div className="card bg-slate-50 my-5 shadow-md rounded-md rounded-t-4xl h-10/12">
+      <h1 className="text-2xl text-gray-700">Change Password</h1>
+      <div className="card bg-slate-50 my-5 shadow-md rounded-md rounded-t-4xl">
         <div className="card-body w-11/12 md:w-3/5 mx-auto">
           <form onSubmit={formik.handleSubmit}>
-            <fieldset className="fieldset w-full">
-              <legend className="fieldset-legend text-slate-800">
-                Old Password
-              </legend>
-              <label className="input input-accent validator w-full">
-                <input
-                  type="password"
-                  name="oldPassword"
-                  id="oldPassword"
-                  onChange={formik.handleChange}
-                  value={formik.values.oldPassword}
-                />
-              </label>
-              {formik.errors.oldPassword && formik.touched.oldPassword && (
-                <div className="feedback text-red-600">
-                  {formik.errors.oldPassword}
-                </div>
-              )}
-            </fieldset>
-            <fieldset className="fieldset w-full">
-              <legend className="fieldset-legend text-slate-800">
-                New Password
-              </legend>
-              <label className="input input-accent validator w-full">
-                <input
-                  type="password"
-                  name="newPassword"
-                  id="newPassword"
-                  onChange={formik.handleChange}
-                  value={formik.values.newPassword}
-                />
-              </label>
-              {formik.errors.newPassword && formik.touched.newPassword && (
-                <div className="feedback text-red-600">
-                  {formik.errors.newPassword}
-                </div>
-              )}
-            </fieldset>
-            <fieldset className="fieldset w-full">
-              <legend className="fieldset-legend text-slate-800">
-                Confirm New Password
-              </legend>
-              <label className="input input-accent validator w-full">
-                <input
-                  type="password"
-                  name="confirmNewPassword"
-                  id="confirmNewPassword"
-                  onChange={formik.handleChange}
-                  value={formik.values.confirmNewPassword}
-                />
-              </label>
-              {formik.errors.confirmNewPassword &&
-                formik.touched.confirmNewPassword && (
-                  <div className="feedback text-red-600">
-                    {formik.errors.confirmNewPassword}
-                  </div>
-                )}
-            </fieldset>
-            <button
-              type="submit"
-              className="btn border-0 bg-emerald-600 hover:bg-emerald-700 active:bg-emerald-700 transition ease-in-out duration-300 text-slate-100 w-full my-5 focus:outline-none"
-            >
-              Submit
-            </button>
+            <FormInput
+              formik={formik}
+              name="oldPassword"
+              label="Old Password"
+              type="password"
+            />
+            <FormInput
+              formik={formik}
+              name="newPassword"
+              label="New Password"
+              type="password"
+            />
+            <FormInput
+              formik={formik}
+              name="confirmNewPassword"
+              label="Confirm New Password"
+              type="password"
+            />
+            <Button type="submit" name="Submit" />
           </form>
         </div>
       </div>
