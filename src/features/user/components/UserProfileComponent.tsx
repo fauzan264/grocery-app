f3325c1316792a8ed39f649de0447fa165cad681
Fauzan <fauzan.c4@gmail.com>
2025-10-05 03:50:33 +0700

    feat: create page create user address

--- src/features/user/components/UserProfileComponent.tsx
+++ src/features/user/components/UserProfileComponent.tsx
@@ -29,42 +29,26 @@ export default function UserProfileComponent({
         defaultChecked
       />
       <div className="tab-content bg-slate-100 p-6">
-        <div className="flex my-2">
-          <div className="flex w-full justify-start md:justify-end">
-            <div
-              className="flex flex-col space-y-2 w-full 
-                    md:flex-row md:space-x-2 md:space-y-0 md:w-auto gap-2"
+        <div className="flex my-2 justify-center md:justify-end">
+          <div className="flex flex-col md:flex-row gap-2 w-4/5 md:w-auto">
+            <Link
+              href={`/admin/profile/change-password`}
+              className="btn btn-sm bg-emerald-500 hover:bg-emerald-600 text-white rounded-md border-0 transition duration-300"
             >
-              <Link
-                href={`/admin/profile/change-password`}
-                className="w-4/5 mx-auto md:w-auto btn btn-sm bg-emerald-500 text-white text-sm rounded-md hover:bg-emerald-600 active:bg-emerald-600 transition ease-in-out duration-300 focus:outline-none focus:ring-0 border-0"
-              >
-                Change Password
-              </Link>
-              <Link
-                href={`/admin/profile/edit`}
-                className="w-4/5 mx-auto md:w-auto btn btn-sm bg-amber-400 text-white text-sm rounded-md hover:bg-amber-500 active:bg-amber-500 transition ease-in-out duration-300 focus:outline-none focus:ring-0 border-0"
-              >
-                Edit
-              </Link>
-            </div>
+              Change Password
+            </Link>
+            <Link
+              href={`/admin/profile/edit`}
+              className="btn btn-sm bg-amber-400 hover:bg-amber-500 text-white rounded-md border-0 transition duration-300"
+            >
+              Edit
+            </Link>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-4">
           <div className="col-span-full md:col-span-1">
-            {/* <figure className="w-40 h-40 block relative rounded-lg">
-              {profile?.photoProfile && (
-                <Image
-                  src={String(profile?.photoProfile)}
-                  alt={`${profile?.fullName} image`}
-                  fill
-                  className="object-cover"
-                />
-              )}
-            </figure> */}
             <div className="col-span-full md:col-span-1 flex flex-col items-start space-y-4 md:pr-6  pb-6 md:pb-0 border-b md:border-b-0">
               <div className="relative mx-auto md:mx-0">
-                {/* <img src="URL_FOTO_PROFIL" alt="Christy JKT48" className="w-24 h-24 rounded-full object-cover shadow-md" /> */}
                 <figure className="w-59 h-59 block relative rounded-3xl overflow-hidden shadow-md">
                   {profile?.photoProfile && (
                     <Image
@@ -129,6 +113,16 @@ export default function UserProfileComponent({
         aria-label="Address"
       />
       <div className="tab-content bg-slate-100 p-6">
+        <div className="flex my-2 justify-center md:justify-end">
+          <div className="flex flex-col md:flex-row gap-2 w-4/5 md:w-auto">
+            <Link
+              href={`/admin/profile/address/create`}
+              className="btn btn-sm bg-emerald-500 hover:bg-emerald-600 text-white rounded-md border-0 transition duration-300"
+            >
+              Create Address
+            </Link>
+          </div>
+        </div>
         <AddressComponent token={token} userId={userId} />
       </div>
     </div>
